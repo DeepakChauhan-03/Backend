7f1be06ddbd4b6defa4b01b5a9014c69a2a10df4
Deepak Chauhan <deepakchauhan6141@gmail.com>
2026-03-06 23:09:04 +0530

    Project day 2

--- GenAi/Frontend/src/App.jsx
+++ GenAi/Frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { RouterProvider } from 'react-router'
+import {router} from './app.routes.jsx'
+import { AuthProvider } from './features/auth/auth.context.jsx'
 
 const App = () => {
   return (
-    <div>
-      hello
+    <div >
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   )
 }
---
+++ GenAi/Frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {useNavigate , Link} from 'react-router'
+
+const Register = () => {
+  const navigate = useNavigate()
+
+    const handleSubmit = (e)=>{
+     e.preventDefault()
+    }
+  return (
+    <div>
+       <div className='h-screen w-full flex flex-col items-center justify-center bg-gray-900'>
+  <form
+  onSubmit={handleSubmit}
+  className='bg-gray-300 p-6 rounded-lg shadow-md flex flex-col gap-4 w-100'>
+    <h1 className='text-3xl font-semibold text-center'>Register</h1>
+
+   <div>
+      <label htmlFor="username">Username</label>
+      <input className='border w-full p-2 rounded mt-1' type="text" id='username' name='username' placeholder='Enter Username'/>
+    </div>
+
+    <div>
+      <label htmlFor="email">Email</label>
+      <input className='border w-full p-2 rounded mt-1' type="text" id='email' name='email' placeholder='Enter e-mail'/>
+    </div>
+
+    <div>
+      <label htmlFor="password">Password</label>
+      <input className='border w-full p-2 rounded mt-1' type="password" id='password' name='password' placeholder='Enter your password'/>
+    </div>
+
+    <button className='bg-green-500 text-white p-2 rounded active:scale-95'>Register</button>
+
+     <p className='mx-14'>Already have an account? <Link to={"/login"} className='text-blue-900 underline'>Login</Link></p>
+  </form>
+ 
+</div>
+    </div>
+  )
+}
+
+export default Register
