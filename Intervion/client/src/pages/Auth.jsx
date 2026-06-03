29eec2433458e246c0ee8792a06388c7361d2312
Deepak Chauhan <deepakchauhan6141@gmail.com>
2026-06-03 23:53:20 +0530

    auth page

---
+++ Intervion/client/src/pages/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div className='w-full min-h-screen bg-[#f3f3f3]  flex items-center justify-center px-6 py-20'>
+      this is auth
+    </div>
+  )
+}
+
+export default Auth
