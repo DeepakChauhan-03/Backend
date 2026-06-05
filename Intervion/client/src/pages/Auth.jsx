1b78a0fd21c6d06709514c906b86a6bda63425a2
Deepak Chauhan <deepakchauhan6141@gmail.com>
2026-06-05 20:31:29 +0530

    Auth page done

--- Intervion/client/src/pages/Auth.jsx
+++ Intervion/client/src/pages/Auth.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { VscRobot } from "react-icons/vsc";
+import { IoSparkles } from "react-icons/io5";
+import { FcGoogle } from "react-icons/fc";
 
 const Auth = () => {
   return (
     <div className='w-full min-h-screen bg-[#f3f3f3]  flex items-center justify-center px-6 py-20'>
-      this is auth
+       <div className='w-full max-w-md p-8 rounded-3xl bg-white shadow-2xl border border-gray-200'>
+            <div className='flex items-center justify-center gap-3 mb-6'>
+                 <div className='bg-black text-white p-2 rounded-lg'>
+                  <VscRobot size={18} /></div>
+                 <h2 className='text-2xl font-semibold'>Intervion.AI</h2>
+            </div>
+            <h1 className='text-2xl md:text-3xl font-semibold text-center leading-snug mb-4'>
+              Continue with
+              <span className='bg-green-100 text-green-600 px-3 py-1 rounded-full inline-flex
+              items-center gap-2'>
+               <IoSparkles size={18} /> AI Smart Interview
+              </span>
+            </h1>
+            <p className='text-gray-500 text-center text-sm md:text-base 
+            leading-relaxed mb-8'> Sign in to start AI powered mock interviews,
+            track your progress, and unlock detailed performance insights.</p>
+
+            <button className='w-full flex items-center justify-center gap-3 py-3 bg-black
+            text-white rounded-full shadow-md hover:scale-102'>
+              <FcGoogle size={20}/> Continue with Google
+              </button>
+       </div>
     </div>
   )
 }
--- test.js
+++ test.js
@@ -1,27 +1,28 @@
-let arr = [1, "X", "X"];
-let numbers = [];
-let romans = [];
-let alphabets = [];
-for (let i = 0; i < arr.length; i++) {
+// let arr = [1, "X", "X"];
+// let numbers = [];
+// let romans = [];
+// let alphabets = [];
+// for (let i = 0; i < arr.length; i++) {
 
-    if (typeof arr[i] === "number") {
-        numbers.push(arr[i]);
-    }
+//     if (typeof arr[i] === "number") {
+//         numbers.push(arr[i]);
+//     }
 
-    else if (typeof arr[i] === "string") {
+//     else if (typeof arr[i] === "string") {
 
-        // Single letter = Alphabet
-        if (arr[i].length === 1) {
-            alphabets.push(arr[i]);
-        }
+//         // Single letter = Alphabet
+//         if (arr[i].length === 1) {
+//             alphabets.push(arr[i]);
+//         }
 
-        // Roman 
-        else if (/^[IVXLCDM]+$/i.test(arr[i])) {
-            romans.push(arr[i]);
-        }
-    }
-}
+//         // Roman 
+//         else if (/^[IVXLCDM]+$/i.test(arr[i])) {
+//             romans.push(arr[i]);
+//         }
+//     }
+// }
 
-console.log("Numbers =", numbers);
-console.log("Romans =", romans);
-console.log("Alphabets =", alphabets);
+// console.log("Numbers =", numbers);
+// console.log("Romans =", romans);
+// console.log("Alphabets =", alphabets);
+
