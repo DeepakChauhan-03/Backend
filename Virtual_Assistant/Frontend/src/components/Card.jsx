470f537574d25ad36874c1df2abcffb2f5a65002
Deepak Chauhan <deepakchauhan6141@gmail.com>
2026-06-23 01:08:31 +0530

    Card

--- Virtual_Assistant/Frontend/src/components/Card.jsx
+++ Virtual_Assistant/Frontend/src/components/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({image}) => {
   return (
-    <div>
-      
+    <div className='w-18 h-35  lg:w-45 lg:h-70 bg-[#030326] border-2 border-[blue] rounded-2xl 
+    overflow-hidden hover:shadow-2xl hover:shadow-blue-500 cursor-pointer
+    hover:border-3 hover:border-white'>
+        <img src={image} className='h-full object-cover' alt="image" />
     </div>
   )
 }
--- Virtual_Assistant/Frontend/src/pages/Customize.jsx
+++ Virtual_Assistant/Frontend/src/pages/Customize.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Card from '../components/Card'
+import image1 from '../assets/jarvisbg.jpg'
+import { LuImageUp } from "react-icons/lu";
 
 const Customize = () => {
   return (
-    <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#020260]'>
+    <div className='w-full h-screen bg-linear-to-t from-[black] to-[#020260]
+    flex items-center justify-center flex-col'>
+     
+     <h1 className='text-white text-5xl pb-10 font-semibold'>Select your
+       <span className='text-blue-200'> Assistant Image</span></h1>
+
+      <div className='w-[90%] max-w-[60%] flex justify-center items-center
+       flex-wrap gap-7'>
+          <Card  image={image1}/>
+          <Card  image={image1}/>
+          <Card  image={image1}/>
+          <Card  image={image1}/>
+          <Card  image={image1}/>
+          <Card  image={image1}/>
+          <div className='w-18 h-35 lg:w-45 lg:h-70 bg-[#030326] border-2 border-[blue] rounded-2xl 
+    overflow-hidden hover:shadow-2xl hover:shadow-blue-500 cursor-pointer
+    hover:border-3 hover:border-white flex items-center justify-center text-4xl'>
+        <LuImageUp  className='text-white'/>
+    </div>
+      </div>
       
     </div>
   )
